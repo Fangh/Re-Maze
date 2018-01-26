272a002ff7c2afa1227dc97606e766459f11d025
Fangh <toto.klein@laposte.net>
2018-01-26 23:35:29 +0100

    FPS player can put decals on walls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour 
+{
+	public GameObject decalPrefab;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			RaycastHit hit;
+			if ( Physics.Raycast(transform.position, transform.forward, out hit, 2) )
+			{
+				GameObject.Instantiate(decalPrefab, hit.point, Quaternion.Euler( hit.normal) );				
+			}
+		}
 		
 	}
 }
