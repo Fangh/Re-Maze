82ab2943684c09192a4a48e3da577e50d3f7fa39
Fangh <toto.klein@laposte.net>
2018-01-27 10:51:41 +0100

    cannot run + getcurrentSteps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour 
@@ -9,24 +9,46 @@ public class PlayerController : MonoBehaviour
 	[Header("References")]
 	public GameObject decalPrefab;
 	public LayerMask decalsStickOn;
-	public Text debugText;
+	public Text stepsText;
+	public Text deadText;
+	public List<Transform> respawnPoints;
 
 	[Header("Balancing")]
 	public float maxSteps = 10;
 
 	private CharacterController controller;
 	private float currentStepsNumber;
+	private bool isDead = false;
 
 	// Use this for initialization
 	void Start () 
 	{
 		controller = GetComponent<CharacterController>();
+		Init();
+	}
+
+	void Init()
+	{
+		isDead = false;
+		currentStepsNumber = 0;
+		deadText.gameObject.SetActive(false);
+		controller.enabled = true;
+		transform.position = respawnPoints[Random.Range(0, respawnPoints.Count)].position;
+		transform.rotation = respawnPoints[Random.Range(0, respawnPoints.Count)].rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetButtonDown("Fire1"))
+		if (isDead)
+		{
+			if ( CrossPlatformInputManager.GetButtonDown("Submit") )
+			{
+				Init();
+			}
+			return;
+		}
+		if (CrossPlatformInputManager.GetButtonDown("Fire1"))
 		{
 			RaycastHit hit;
 			if ( Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 2, decalsStickOn) )
@@ -43,9 +65,21 @@ public class PlayerController : MonoBehaviour
 
 		if ( currentStepsNumber > maxSteps )
 		{
-			SceneManager.LoadScene(0);
+			Die();
 		}
 
-		debugText.text = string.Format("Steps : {0}", Mathf.RoundToInt(currentStepsNumber).ToString());
+		stepsText.text = string.Format("Steps : {0} / {1}", Mathf.RoundToInt(currentStepsNumber).ToString(), maxSteps);
+	}
+
+	void Die()
+	{
+		isDead = true;
+		deadText.gameObject.SetActive(true);
+		controller.enabled = false;
+	}
+
+	public int GetCurrentSteps()
+	{
+		return Mathf.RoundToInt(currentStepsNumber);
 	}
 }
